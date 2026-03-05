2e659ba32916c5ae924bb75e1fcbe39271f1ba4d
Jitendrakumar Prajapati <jitforjava@gmail.com>
2026-03-05 12:47:02 +0530

    setup

--- src/main/java/com/example/doccluster/service/ClusteringService.java
+++ src/main/java/com/example/doccluster/service/ClusteringService.java
@@ -10,7 +10,7 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.util.NamedList;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.io.IOException; 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
